2ecafd842738f32ca383bd1966e812e73ac3711a
David Kurek <david.kurek@hotmail.de>
2025-08-16 18:58:10 +0200

    chore: changed  io to check if leaving from bottom or top

--- utilities/intersectionObserver.js
+++ utilities/intersectionObserver.js
@@ -1,21 +1,45 @@
 const intersectionObserver = (target, options = {}) => {
-  const sun = target.querySelector("#sun");
-  const horizon = target.querySelector("#horizon");
-  const reflection = target.querySelector("#reflection");
+	const sun = target.querySelector('#sun');
+	const horizon = target.querySelector('#horizon');
+	const reflection = target.querySelector('#reflection');
+	const targets = [sun, horizon, reflection];
 
-  const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      console.log("observer callback");
-      console.log("is intersecting:", entry.isIntersecting);
-      console.log(entry.boundingClientRect);
-      console.log(entry.intersectionRect);
-      if (entry.isIntersecting) {
-        console.log(sun);
-      }
-    });
-  };
+	const callback = (entries) => {
+		for (const entry of entries) {
+			if (entry.isIntersecting) {
+				// Element is (again) intersecting
+				sun.style.animationPlayState = 'running';
+				horizon.style.animationPlayState = 'running';
+				reflection.style.animationPlayState = 'running';
+				console.log('is intersecting');
+			} else if (entry.intersectionRatio === 0) {
+				// Element has left visible area completely
 
-  return new IntersectionObserver(callback, options);
+				console.log('sun is leaving visible area');
+				//sun.style.animationPlayState = 'paused';
+
+				// Optional: determine direction (up/down)
+				// only makes sense if rootBounds exists (when root != null or in the viewport)
+				if (entry.rootBounds) {
+					const leavingTop =
+						entry.boundingClientRect.top < entry.rootBounds.top;
+					const leavingBottom =
+						entry.boundingClientRect.bottom > entry.rootBounds.bottom;
+					// do something with leavingTop / leavingBottom
+					console.log('leavingTop:', leavingTop, 'leavingBottom:', leavingBottom);
+          if (leavingTop || leavingBottom) {
+            // Handle the case where the element is leaving the viewport
+            console.log(document.getAnimations())
+          }
+				}
+			}
+		}
+	};
+
+	const observer = new IntersectionObserver(callback, options);
+	for (const targetElement of targets) {
+		observer.observe(targetElement);
+	}
 };
 
 export default intersectionObserver;
