24a5ad5c8942e78855412c85805ea3576b9936a5
David Kurek <david.kurek@hotmail.de>
2025-08-18 00:03:40 +0200

    chore: added motion class to port kf animations to WAAPI

--- src/RisingSun.js
+++ src/RisingSun.js
@@ -1,61 +1,90 @@
-import styles from "./css/style.css?inline";
-import registerCustomCSSProps from "../utilities/registerCustomCSSProps";
-import intersectionObserver from "../utilities/intersectionObserver";
+import styles from './css/style.css?inline';
+import Motion from '../utilities/motion.js';
+import registerCustomCSSProps from '../utilities/registerCustomCSSProps';
+import IntersectionObserverUtil from '../utilities/intersectionObserver';
+
 
 export class RisingSun extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
+	#hostStyles;
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
 
-  get width() {
-    return this.getAttribute("width");
-  }
+	get width() {
+		return (
+			this.getAttribute('width') ||
+			this.#hostStyles.getPropertyValue('--host-width').trim()
+		);
+	}
 
-  get primaryColor() {
-    return this.getAttribute("primary-color");
-  }
+	get primaryColor() {
+		return (
+			this.getAttribute('primary-color') ||
+			this.#hostStyles.getPropertyValue('--primary-color').trim()
+		);
+	}
 
-  get secondaryColor() {
-    return this.getAttribute("secondary-color");
-  }
+	get secondaryColor() {
+		return (
+			this.getAttribute('secondary-color') ||
+			this.#hostStyles.getPropertyValue('--secondary-color').trim()
+		);
+	}
 
-  get rootMargin() {
-    return this.getAttribute("root-margin");
-  }
+	connectedCallback() {
+		registerCustomCSSProps();
 
-  get scrollMargin() {
-    return this.getAttribute("scroll-margin");
-  }
+		this.shadowRoot.innerHTML = /* HTML */ `
+			<style>
+				${styles}
+			</style>
+			<div id="container">
+				<div id="sun"></div>
+				<div id="background"></div>
+				<div id="horizon"></div>
+				<div id="reflection"></div>
+			</div>
+		`;
 
-  get threshold() {
-    return this.getAttribute("threshold");
-  }
+		this.#hostStyles = getComputedStyle(this);
+		const container = this.shadowRoot.querySelector('#container');
+    const sun = this.shadowRoot.querySelector('#sun');
+    const horizon = this.shadowRoot.querySelector('#horizon');
+    const reflection = this.shadowRoot.querySelector('#reflection');
+		const rootMarginFromViewport =
+			parseInt(this.width) - window.innerHeight / 2;
 
-  connectedCallback() {
-    registerCustomCSSProps();
+		const observer = new IntersectionObserverUtil(container, {
+			root: null,
+			rootMargin: this.rootMargin || `${rootMarginFromViewport}px 0px ${rootMarginFromViewport}px 0px`,
+			threshold: [0, 0.8]
+		});
 
-    this.shadowRoot.innerHTML = /* HTML */ `
-      <style>
-        ${styles}
-      </style>
-      <div id="sun"></div>
-      <div id="background"></div>
-      <div id="horizon"></div>
-      <div id="reflection"></div>
-    `;
+    observer.onIntersect((entry) => { 
+      console.log('is intersecting', entry);
+        sun.style.animationPlayState = 'running';
+        horizon.style.animationPlayState = 'running';
+        reflection.style.animationPlayState = 'running';
+    })
 
-    const observer = intersectionObserver(this.shadowRoot, {
-      root: null,
-      rootMargin: this.rootMargin || "0px",
-      scrollMargin: this.scrollMargin || "0px",
-      threshold: this.threshold || 0.5,
+    observer.onOutersect(({entry, leavingTop, leavingBottom}) => {
+      console.log('is leaving', entry);
+      console.log('is leaving top', leavingTop);
+      console.log('is leaving bottom', leavingBottom);
+      sun.style.animationDirection = 'reverse';
+      horizon.style.animationDirection = 'reverse';
+      reflection.style.animationDirection = 'reverse';
     });
 
-    this.style.setProperty("--host-width", this.width);
-    this.style.setProperty("--primary-color", this.primaryColor);
-    this.style.setProperty("--secondary-color", this.secondaryColor);
-  }
+    observer.observe()
+
+    console.log(observer)
+
+		this.style.setProperty('--host-width', this.width);
+		this.style.setProperty('--primary-color', this.primaryColor);
+		this.style.setProperty('--secondary-color', this.secondaryColor);
+	}
 }
 
-customElements.define("rising-sun", RisingSun);
+customElements.define('rising-sun', RisingSun);
